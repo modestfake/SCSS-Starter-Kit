48f8e31a077f278320a9f63eb7034c718b15d67f
Vitalii Saienko <saienkovitalii@gmail.com>
2017-03-03 23:36:55 +0200

    Add FTP sync

---
+++ config.js
@@ -0,0 +1,9 @@
+const settings = {
+  host:     'agropo00.ftp.ukraine.com.ua',
+  user:     'agropo00_ftp',
+  password: '8l05NcD9tM',
+  path:     'agrokontakt.in.ua/www/test/',
+  url:      'http://agrokontakt.in.ua/test/index.html'
+};
+
+exports.settings = settings;
--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,59 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename       = require('gulp-rename');
 const sourcemaps   = require('gulp-sourcemaps');
 const browserSync  = require('browser-sync').create();
-
+const gutil        = require('gulp-util');
+const ftp          = require('vinyl-ftp');
+const config       = require('./config.js');
 
 gulp.task('sass', function () {
   return gulp.src('./styles/scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions']
-        }))
-        .pipe(rename('style.css'))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./styles/css'));
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions']
+    }))
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./styles/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./styles/scss/**/*.scss', ['sass']);
+gulp.task('deploy-dev', ['sass'], function() {
+  const conn = ftp.create({
+    host:     config.settings.host,
+    user:     config.settings.user,
+    password: config.settings.password,
+    parallel: 10,
+    log:      gutil.log
+  });
+
+  const globs = [
+    '**/*',
+    '*',
+    '!node_modules/**',
+    '!node_modules',
+    '!.git/**'
+  ];
+
+  return gulp.src(globs, {base: '.', buffer: false})
+    .pipe(conn.newer(config.settings.path))
+    .pipe(conn.dest(config.settings.path));
 });
 
-gulp.task('bs', function(){
+gulp.task('sass:watch', ['deploy-dev'], function (done) {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('serve', ['deploy-dev'], function () {
   browserSync.init({
-    proxy: 'http://odezhinka.com/',
+    proxy: config.settings.url,
     port: 3000,
     open: 'external',
     notify: true,
-    ghost: true,
-    files: ['./styles/scss/**/*.scss']
+    ghost: true
   });
 
-  browserSync.watch(['./styles/scss/**/*.scss']).on('change', browserSync.reload);
+  gulp.watch('./styles/scss/**/*.scss', ['sass:watch']);
 });
 
-gulp.task('default', ['sass:watch', 'bs']);
+gulp.task('default', ['serve']);
