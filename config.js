6f71114c0cfd114add9eed8247c5c781a8b94221
Vitalii Saienko <modestfake@gmail.com>
2017-06-20 23:31:15 +0300

    Ignore css, remove ftp configs

--- config.js
+++
@@ -1,9 +0,0 @@
-const settings = {
-  host:     'host',
-  user:     'user',
-  password: 'password',
-  path:     'agrokontakt.in.ua/www/test/',
-  url:      'http://agrokontakt.in.ua/test/index.html'
-};
-
-exports.settings = settings;
