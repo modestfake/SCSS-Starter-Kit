84782b70c6af4a023e89423d1296c3ed38eb33bd
Vitalii Saienko <modestfake@gmail.com>
2017-08-28 00:40:58 +0300

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,53 @@
-'use strict';
-
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
-const clean = require('del');
-
-process.env.NODE_ENV = 'development';
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const sourcemaps = require('gulp-sourcemaps')
+const clean = require('del')
+const svgSprite = require('gulp-svg-sprite')
+const gutil = require('gulp-util')
+process.env.NODE_ENV = 'development'
 
 // Path, related to the root,
 // where both .css and .scss style files
 // will be stored
-const path = './styles/';
+const path = './styles'
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   if (process.env.NODE_ENV === 'development') {
-    return gulp.src(path + 'scss/main.scss')
+    return gulp.src(`${path}/scss/main.scss`)
       .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sass({ outputStyle: 'compressed' })
+        .on('error', sass.logError))
       .pipe(rename('style.css'))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(path + 'css'));
+      .pipe(gulp.dest(`${path}/css`))
   }
 
-  return gulp.src(path + 'scss/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  return gulp.src(`${path}/scss/main.scss`)
+    .pipe(sass({ outputStyle: 'compressed' })
+      .on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 5 versions']
     }))
     .pipe(rename('style.css'))
-    .pipe(gulp.dest(path + 'css'));
-});
+    .pipe(gulp.dest(`${path}/css`))
+})
 
-gulp.task('sass:watch', function () {
-  watch(path + 'scss/**/*.scss', function () {
+gulp.task('sass:watch', () => {
+  watch(`${path}/scss/**/*.scss`, () => {
     gulp.start('sass')
-  });
-});
+  })
+})
 
-gulp.task('build', function () {
-  process.env.NODE_ENV = 'production';
+gulp.task('build', () => {
+  process.env.NODE_ENV = 'production'
 
-  clean(path + 'css/style.css.map')
-    .then(function () {
-      gulp.start('sass');
-    });
-});
+  clean(`${path}/css/style.css.map`)
+    .then(() => {
+      gulp.start('sass')
+    })
+})
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass:watch'])
